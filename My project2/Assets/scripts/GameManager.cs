53d6e73715826da526d557dc30e55e0cf04bd7da
Zaeem944 <25100071@lums.edu.pk>
2024-08-05 12:28:49 +0500

    added menu
    added further menu and game manager

---
+++ My project2/Assets/scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameObject startingUI;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        ShowStartingUI();
+    }
+
+    public void StartGame()
+    {
+        // Hide the starting UI and load the first game scene
+        HideStartingUI();
+        SceneManager.LoadScene("level1"); // Make sure to replace "Level1" with your actual scene name
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is quitting...");
+    }
+
+    private void ShowStartingUI()
+    {
+        if (startingUI != null)
+        {
+            startingUI.SetActive(true);
+        }
+    }
+
+    private void HideStartingUI()
+    {
+        if (startingUI != null)
+        {
+            startingUI.SetActive(false);
+        }
+    }
+}
